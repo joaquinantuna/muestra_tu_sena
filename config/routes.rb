6290c7e77332a53cfd4c7fbf8e47b044bc94131c
Maria AvendaÃno <los.valenzuelas@gmail.com>
2021-09-13 09:50:05 -0300

    agregar about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
-  get "lessons", to: "lessons#index"
   root to: 'lessons#index'
+  get "lessons", to: "lessons#index"
   get "lessons/:id", to: "lessons#show", as: :lesson
-
+  get "about", to: "pages#about"
 end
