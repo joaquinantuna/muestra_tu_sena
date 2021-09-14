bcc471b800ab564d449956f203e88117c9ec1c55
Joaquín Antuña <joaquinantuna@gmail.com>
2021-09-13 22:34:29 -0300

    integracion fotos y video con cloudinary, active records y seed

--- db/migrate/20210909135259_create_lessons.rb
+++ db/migrate/20210909135259_create_lessons.rb
@@ -2,6 +2,8 @@ class CreateLessons < ActiveRecord::Migration[6.0]
   def change
     create_table :lessons do |t|
       t.string :name
+      t.string :photo
+      t.string :video
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
+require 'open-uri'
+Session.destroy_all
+Lesson.destroy_all
+User.destroy_all
+
+
+
 lesson_1 = Lesson.new(
   name: "Vocales",
+  photo: "f",
+  video: "v"
 )
+file_foto = URI.open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631538684/vocales_foto.png")
+file_video = URI.open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1631546178/vocales_video.mp4")
 
+lesson_1.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
+lesson_1.video.attach(io: file_video, filename: 'video.mp4', content_type: 'video/mp4')
 lesson_1.save!
 
 user_1 = User.create(
