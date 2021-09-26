ed60b681d70edbaee48e7448cab7daf65f3f3649
Joaquín Antuña <joaquinantuna@gmail.com>
2021-09-26 15:48:51 -0300

    errores varios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-   def configure_permitted_parameters
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nombres, :apellidos, :direccion,
+    devise_parameter_sanitizer.permit(:sign_up, keys:[  :nombres, :apellidos, :direccion,
                                                         :ciudad, :telefono, :condicion,
                                                         :edad])
 
