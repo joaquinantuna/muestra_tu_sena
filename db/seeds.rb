ed6caeb7b97033ff13441e83be5d1ab1c8947b04
Maria AvendaÃno <los.valenzuelas@gmail.com>
2021-09-30 11:13:38 -0300

    modificar y footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@ User.destroy_all
 lesson_1 = Lesson.new(
   name: "Vocales",
 )
-file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631722650/Lessons/Vocales/vocales_foto.jpg")
+
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1632944657/Lessons/Vocales/vocales_1_uvxhbu.jpg")
 file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1631696555/Lessons/Vocales/vocales_video.mp4")
 
 lesson_1.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
@@ -26,7 +27,7 @@ lesson_2.save!
 lesson_3 = Lesson.new(
   name: "Colores",
 )
-file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631697903/Lessons/Colores/colores_foto.jpg")
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631744297/Lessons/Colores/colores_fot.jpg")
 file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1631704067/Lessons/Colores/Colores_video.mp4")
 
 lesson_3.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
@@ -36,7 +37,7 @@ lesson_3.save!
 lesson_4 = Lesson.new(
   name: "Familia",
 )
-file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631722621/Lessons/Familia/Familia_foto.jpg")
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1632880759/Lessons/Familia/familia_fotos.jpg")
 file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1631837624/Lessons/Familia/familia_video.mp4")
 
 lesson_4.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
@@ -46,12 +47,33 @@ lesson_4.save!
 lesson_5 = Lesson.new(
   name: "Numeros",
 )
-file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1631545384/Lessons/Numeros/numeros_foto_leccion.jpg")
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1632986331/Lessons/Numeros/numeros_fotos.gif")
 file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1632829498/Lessons/Numeros/Numeros_video.mp4")
 
 lesson_5.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
 lesson_5.video.attach(io: file_video, filename: 'video.mp4', content_type: 'video/mp4')
 lesson_5.save!
+
+lesson_6 = Lesson.new(
+  name: "Estaciones",
+)
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1632867498/Lessons/estaciones/estaciones_foto.jpg")
+file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1632865509/Lessons/estaciones/Estaciones_video.mp4")
+
+lesson_6.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
+lesson_6.video.attach(io: file_video, filename: 'video.mp4', content_type: 'video/mp4')
+lesson_6.save!
+
+
+lesson_7 = Lesson.new(
+  name: "Emociones",
+)
+file_foto = open("https://res.cloudinary.com/muestra-tu-sena/image/upload/v1632952055/Lessons/Otros/emociones_foto.jpg")
+file_video = open("https://res.cloudinary.com/muestra-tu-sena/video/upload/v1632951813/Lessons/Otros/Emociones_video.mp4")
+
+lesson_7.photo.attach(io: file_foto, filename: 'foto.png', content_type: 'image/png')
+lesson_7.video.attach(io: file_video, filename: 'video.mp4', content_type: 'video/mp4')
+lesson_7.save!
 
 user_1 = User.create(
   email: "user1@email.com",
