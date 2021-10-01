f1fd7d13af9f47e5581daf13219d0a00af3f2a9f
Joaquín Antuña <joaquinantuna@gmail.com>
2021-10-01 01:53:19 -0300

    configurando mail en prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,21 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+# config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.smtp_settings = {
+:address => "smtp.gmail.com",
+:port => 587,
+:domain => "mail.google.com",
+:user_name => "followersmktg@gmail.com",
+:password => "petudogs",
+:authentication => :plain,
+:enable_starttls_auto => true
+}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
